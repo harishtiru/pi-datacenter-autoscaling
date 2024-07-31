fdc345ead1a4fad1d0edd880d165fd35f6fa73aa
harishtiru <harish.t@pidatacenters.com>
2024-07-31 13:16:19 +0530

    test

--- get_highest_hostname_prefix.sh
+++ get_highest_hostname_prefix.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# Determine which file to use
-if [[ -f "ansible/inventory.ini" ]]; then
-  FILE="ansible/inventory.ini"
-else
-  FILE="ansible/inventory.ini.old"
-fi
+# Get the highest hostname prefix
+highest_prefix=$(awk 'NF==2{print $1}' ansible/inventory.ini.old | grep -o '[a-zA-Z]*' | tail -1)
 
-# Process the chosen file
-highest_prefix=$(awk '
-/^\[workernodes\]/ {
-  # Print lines under [workernodes] and [vmnames] sections
-  if (/^\[workernodes\]/) {
-    print $0
-    # Read the lines under the current section until the next section starts or the file ends
-    while ((getline line) > 0 && line !~ /^\[/) {
-	    if (line != "") print line # Remove blank lines
-    }
-  }
-}' "$FILE" | awk 'NF==2{print $1}'|tail -1| grep -o '[a-zA-Z]*')
-# Output the result in JSON format
+# Output JSON format
 echo "{ \"output\": \"$highest_prefix\" }"
--- get_highest_vm_name_prefix.sh
+++ get_highest_vm_name_prefix.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# Determine which file to use
-if [[ -f "ansible/inventory.ini" ]]; then
-  FILE="ansible/inventory.ini"
-else
-  FILE="ansible/inventory.ini.old"
-fi
+# Get the highest VM name prefix
+highest_vm_prefix=$(awk -F'=' '/worker[0-9]+_name=/ {gsub("[^[:alpha:]]", "", $2); print $2}' ansible/inventory.ini.old | sort -n | tail -1)
 
-# Process the chosen file
-awk '
-/^\[vmnames\]/ {
-  # Print lines under [workernodes] and [vmnames] sections
-  if (/^\[vmnames\]/) {
-    print $0
-    # Read the lines under the current section until the next section starts or the file ends
-    while ((getline line) > 0 && line !~ /^\[/) {
-      if (line != "") print line # Remove blank lines
-    }
-  }
-}
-' "$FILE" | cut -f 2 -d= | grep -v "vmnames" | tail -1 | grep -o '[a-zA-Z]*'
+# Output JSON format
+echo "{ \"output\": \"$highest_vm_prefix\" }"
